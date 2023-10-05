08c998e8c8b93e8405261cf06e2c2b80ca13b62e
Ragib Noor <ragib.noor@iqvia.com>
2023-10-05 14:52:06 +0600

    add logger

---
+++ EmbedIO/LogExtention.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EmbedIO
+{
+    public static class LogExtention
+    {
+        public static void Info(
+            this string message,
+            string? source = null)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine(source);
+        }
+
+        public static void Debug(
+            this string message,
+            string? source = null)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine(source);
+        }
+
+        public static void Error(
+            this string message,
+            string? source = null)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine(source);
+        }
+
+        public static void Trace(
+            this string message,
+            string? source = null)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine(source);
+        }
+
+    }
+}
