08ff334894f7120db48b4e56b19561beda2dc0a0
Ragib Noor <>
2023-12-20 15:21:07 +0600

    fix build issue with a splash screen

--- XamarinApp/XamarinApp.iOS/AppDelegate.cs
+++ XamarinApp/XamarinApp.iOS/AppDelegate.cs
@@ -1,13 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
+using System.Globalization;
+using System.Threading;
+using CoreFoundation;
 using Foundation;
 using UIKit;
+using Xam.Plugin.WebView.Abstractions;
 using Xam.Plugin.WebView.iOS;
+using Xamarin.Forms;
+using XamarinApp;
 
+[assembly: Preserve(typeof(FormsWebView), AllMembers = true)]
+[assembly: Preserve(typeof(FormsNavigationDelegate), AllMembers = true)]
+[assembly: Preserve(typeof(FormsWebViewRenderer), AllMembers = true)]
+[assembly: Preserve(typeof(CFNotificationCenter), AllMembers = true)]
 [assembly: Preserve(typeof(SQLitePCL.Batteries), AllMembers = true)]
-namespace XamarinApp.iOS
+
+namespace OfflineApp.iOS
 {
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
     // User Interface of the application, as well as listening (and optionally responding) to 
@@ -25,12 +33,56 @@ namespace XamarinApp.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             SQLitePCL.Batteries.Init();
-
             FormsWebViewRenderer.Initialize();
             global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App());
+            Window = new UIWindow(UIScreen.MainScreen.Bounds);
+            var controller = new ViewController();
+            controller.View.BackgroundColor = UIColor.White;
+            Window.RootViewController = controller;
 
-            return base.FinishedLaunching(app, options);
+            Window.MakeKeyAndVisible();
+            RegisterLockScreenAbort();
+            MessagingCenter.Subscribe<object, object>(this, "ShowMainScreen", (sender, args) =>
+            {
+                LoadApplication(new App());
+                base.FinishedLaunching(app, options);
+                CultureInfo cultureInfo = CultureInfo.InvariantCulture;
+                System.Globalization.CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+                System.Globalization.CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+                Thread.CurrentThread.CurrentCulture = cultureInfo;
+            });
+           
+
+            return true;
         }
+        public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
+        {
+            if (Xamarin.Forms.Application.Current is App myApp && null != url)
+            {
+               // myApp.OnAppLinkRequestReceived(new Uri(url.AbsoluteString));
+            }
+            return true;
+        }
+        public override UIInterfaceOrientationMask GetSupportedInterfaceOrientations(UIApplication application, UIWindow forWindow)
+        {
+            return UIInterfaceOrientationMask.All;
+        }
+
+        private void RegisterLockScreenAbort()
+        {
+            // Start- Code to detect lock screen 
+            const string notificationName = "com.apple.springboard.lockcomplete";
+            var token = CFNotificationCenter.Darwin.AddObserver(
+                name: notificationName,
+                objectToObserve: null,
+                notificationHandler: (name, userInfo) => {
+                    Thread.CurrentThread.Abort();
+                },
+                suspensionBehavior: CFNotificationSuspensionBehavior.DeliverImmediately);
+            // End- Code to cancel lock screen
+        }
+
+       
+
     }
 }
